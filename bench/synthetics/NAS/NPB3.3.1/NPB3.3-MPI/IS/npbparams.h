4b2b1fb74df8a3f7f6682132c16808289d45905e
Jorge Naranjo <naranjja@ct001.noor-int.kaust.edu.sa>
2012-09-25 16:37:20 +0300

    bench dirs added

---
+++ bench/synthetics/NAS/NPB3.3.1/NPB3.3-MPI/IS/npbparams.h
@@ -0,0 +1,15 @@
+#define CLASS 'A'
+#define NUM_PROCS 4
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.   */
+   
+#define COMPILETIME "12 Sep 2012"
+#define NPBVERSION "3.3.1"
+#define MPICC "mpicc"
+#define CFLAGS "-O2"
+#define CLINK "$(MPICC)"
+#define CLINKFLAGS "-O2"
+#define CMPI_LIB "# -L/usr/local/lib -lmpi"
+#define CMPI_INC "-I/usr/local/include"
